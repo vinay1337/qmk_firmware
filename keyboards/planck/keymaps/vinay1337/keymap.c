c1ae52e7e7c12e7b73402f870e651e42e970add1
Vinay <vjanard@gmail.com>
2021-02-15 18:10:49 -0600

    Improved nav layer and added some hyphens

--- keyboards/planck/keymaps/vinay1337/keymap.c
+++ keyboards/planck/keymaps/vinay1337/keymap.c
@@ -125,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB , KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     ESC_NAV, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, ENTRSFT,
-    TD_CTRL, KC_LCTL, KC_LGUI, KC_LALT, RAISE,   KC_SPC,  KC_SPC,  LOWER,   KC_RCTL, KC_RGUI, KC_RALT, ONESHFT
+    TD_CTRL, KC_LCTL, KC_LGUI, KC_LALT, RAISE,   KC_SPC,  KC_SPC,  LOWER,   KC_RCTL, KC_RGUI, KC_RALT, NAVIG
 ),
 
 /* Lower
@@ -185,9 +185,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 [_MOVE] = LAYOUT_planck_grid(
-    KC_BTN2, _______, KC_MS_U, _______, KC_WH_U, _______, _______, _______, _______, _______, _______, KC_DEL ,
+    _______, _______, KC_MS_U, _______, KC_WH_U, _______, _______, _______, _______, KC_MINS, KC_EQL, KC_DEL ,
     _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_COLN, KC_DQUO,
-    _______, _______, _______, _______, _______, KC_HOME, KC_END , _______, _______, _______, _______, _______,
+    _______, _______, _______, KC_WH_L, KC_WH_R, KC_HOME, KC_END , _______, _______, _______, _______, _______,
     _______, _______, KC_BTN3, KC_BTN2, KC_BTN1, _______, _______, _______, _______, _______, _______, _______
 )
 
